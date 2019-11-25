ecc42778797006cc2525fbec2a6264ca2d7d4d03
memset0 <memset0@outlook.com>
2019-11-25 23:37:20 +0800

    add comments

--- modules/SYZOJSpider.js
+++ modules/SYZOJSpider.js
@@ -1,4 +1,8 @@
 module.exports = class {
+  /**
+   * SYZOJ Spider (by memset0)
+   * 适用于 LibreOJ 及其他 SYZOJ 的第三方部署版本
+   */
   console_log(res) {
     console.log(`[${this.name} Spider] ` + res)
   }
@@ -8,9 +12,14 @@ module.exports = class {
   render_string_to_markdown(source){
     return source;
   }
-  login(username, password) {
+  /**
+   * 登录
+   * @param {string} username 用户名
+   * @param {string} password 密码
+   */
+  async login(username, password) {
     password = md5(password + 'syzoj2_xxx');
-    this.agent
+    await this.agent
       .post(`${this.host}/api/login`)
       .send({
         'username': username,
@@ -35,8 +44,15 @@ module.exports = class {
         }
       });
   }
+  /**
+   * 爬取题目内容
+   * @param {int} problem 题目编号
+   * @param {path} destination 目标目录
+   * @param {function} callback 回调函数，可用于修改题目信息
+   */
   async get(problem, destination, callback) {
     let result = new ProblemClass();
+    // 通过 export 接口爬取题目信息
     await this.agent
       .get(`${this.host}/problem/${problem}/export`)
       .then(res => {
@@ -64,63 +80,87 @@ module.exports = class {
         if (result.limit_and_hint) result.markdown += '### 数据范围与提示\n\n' + result.limit_and_hint + '\n\n';
       });
     result = callback(result)
+    // 将结果写入文件
     fs.mkdirsSync(destination);
     fs.writeFileSync(path.join(destination, 'main.json'), JSON.stringify(result));
     fs.writeFileSync(path.join(destination, 'statement.md'), result.markdown);
     this.console_log('题面下载完毕')
   }
+  /**
+   * 爬取题目数据
+   * @param {int} problem 题目编号
+   * @param {path} destination 目标目录
+   */
   async data(problem, destination) {
     fs.mkdirsSync(destination);
+    // 下载题面给定的数据
     await this.agent
       .get(`${this.host}/problem/${problem}/testdata/download`)
       .then(res => {
         fs.writeFileSync(path.join(destination, 'down.zip'), res.body)
         this.console_log('数据下载完毕')
-        let zip = new adm_zip(path.join(destination, 'down.zip'));
-        fs.mkdirsSync(path.join(destination, 'down'));
-        zip.extractAllTo(path.join(destination, 'down'), true);
-        let filelist = fs.readdirsSync(path.join(destination, 'down'));
-        let data_counter = 0
-        let addTestData = (input, output) => {
-          data_counter++
-          fs.writeFileSync(path.join(destination, `data${data_counter}.in`), fs.readFileSync(input))
-          fs.writeFileSync(path.join(destination, `data${data_counter}.out`), fs.readFileSync(output))
-        }
-        filelist.forEach(e => {
-          if (path.extname(e) == '.in') {
-            if (filelist.indexOf(e.replace(/.in$/g, '.out')) != -1) {
-              addTestData(e, e.replace(/.in$/g, '.out'))
-            }
-            if (filelist.indexOf(e.replace(/.in$/g, '.ans')) != -1) {
-              addTestData(e, e.replace(/.in$/g, '.ans'))
-            }
-          }
-        })
-        fs.rmdirsSync(path.join(destination, 'down'))
-        fs.rmdirsSync(path.join(destination, 'down.zip'))
-        if (fs.existsSync(path.join(destination, 'data.zip'))) {
-          fs.rmdirsSync(path.join(destination, 'data.zip'))
-        }
-        let statement_information = JSON.parse(fs.readFileSync(path.join(destination, 'main.json')))
-        let uoj_config = ''
-        uoj_config += 'use_builtin_judger on\nuse_builtin_checker ncmp\n'
-        uoj_config += `n_tests ${data_counter}\nn_ex_tests 0\nn_sample_tests 0\n`
-        uoj_config += 'input_pre data\ninput_suf in\n'
-        uoj_config += 'output_pre data\noutput_suf out\n'
-        uoj_config += `time_limit ${Math.ceil(statement_information.time_limit / 1000.0)}\n`
-        uoj_config += `memory_limit ${statement_information.memory_limit}\n`
-        fs.writeFileSync(path.join(destination, `problem.conf`), uoj_config)
-        zip = new adm_zip();
-        zip.addLocalFolder(destination);
-        zip.writeZip(path.join(destination, 'data.zip'));
-        this.console_log('数据压缩完毕')
       })
+    // 解压缩数据并完成解析配置
+    let zip = new adm_zip(path.join(destination, 'down.zip'));
+    fs.mkdirsSync(path.join(destination, 'down'));
+    zip.extractAllTo(path.join(destination, 'down'), true);
+    let filelist = fs.readdirsSync(path.join(destination, 'down'));
+    let data_counter = 0
+    let addTestData = (input, output) => {
+      data_counter++
+      fs.writeFileSync(path.join(destination, `data${data_counter}.in`), fs.readFileSync(input))
+      fs.writeFileSync(path.join(destination, `data${data_counter}.out`), fs.readFileSync(output))
+    }
+    filelist.forEach(e => {
+      if (path.extname(e) == '.in') {
+        if (filelist.indexOf(e.replace(/.in$/g, '.out')) != -1) {
+          addTestData(e, e.replace(/.in$/g, '.out'))
+        }
+        if (filelist.indexOf(e.replace(/.in$/g, '.ans')) != -1) {
+          addTestData(e, e.replace(/.in$/g, '.ans'))
+        }
+      }
+    })
+    // 删除无用文件
+    fs.rmdirsSync(path.join(destination, 'down'))
+    fs.rmdirsSync(path.join(destination, 'down.zip'))
+    if (fs.existsSync(path.join(destination, 'data.zip'))) {
+      fs.rmdirsSync(path.join(destination, 'data.zip'))
+    }
+    let statement_information = JSON.parse(fs.readFileSync(path.join(destination, 'main.json')))
+    // 生成 UOJ 格式的数据配置文件
+    let uoj_config = ''
+    uoj_config += 'use_builtin_judger on\nuse_builtin_checker ncmp\n'
+    uoj_config += `n_tests ${data_counter}\nn_ex_tests 0\nn_sample_tests 0\n`
+    uoj_config += 'input_pre data\ninput_suf in\n'
+    uoj_config += 'output_pre data\noutput_suf out\n'
+    uoj_config += `time_limit ${Math.ceil(statement_information.time_limit / 1000.0)}\n`
+    uoj_config += `memory_limit ${statement_information.memory_limit}\n`
+    fs.writeFileSync(path.join(destination, `problem.conf`), uoj_config)
+    // 压缩数据、配置文件和题面内容
+    zip = new adm_zip();
+    zip.addLocalFolder(destination);
+    zip.writeZip(path.join(destination, 'data.zip'));
+    this.console_log('数据压缩完毕')
   }
-  async download(problem, destination, callback) {
+  /**
+   * 爬取题目
+   * @param {int} problem 题目编号
+   * @param {path} destination 目标地址，用于保存爬取的数据
+   * @param {function} callback 用于 get 成员函数的回调
+   */
+  async download(problem, destination, callback = e => e) {
     this.console_log('下载题目 ' + problem)
     await this.get(problem, destination, callback);
     await this.data(problem, destination);
   }
+  /**
+   * constructor
+   * @param {string} name OJ 名称
+   * @param {url} host OJ 地址
+   * @param {string} username 用户名，如果不登录则无需配置
+   * @param {string} password 密码，如果不登录则无需配置
+   */
   constructor(name, host, username=null, password=null) {
     this.name = name;
     this.host = host;
--- modules/UOJUploader.js
+++ modules/UOJUploader.js
@@ -1,90 +1,118 @@
 module.exports = class {
+  /**
+   * UOJ Uploader (by memset0)
+   * 适用于 UOJ 社区版
+   */
   console_log(res) {
     console.log(`[${this.name} Uploader] ` + res)
   }
   throw_error(res) {
     throw new Error(`[${this.name} Uploader] ` + res);
   }
+  /**
+   * 登录
+   * @param {string} username 用户名
+   * @param {string} password 密码
+   */
   async login(username, password) {
     this.agent
-    .get(`${this.host}/login`)
-    .then(res => {
-      let csrf_re = /_token : "([\S]+)",/g
-      let csrf = csrf_re.exec(res.text)[1]
-      let salt_re = /password : md5\(\$\('#input-password'\).val\(\), "([\S]+)"\)/g
-      let salt = salt_re.exec(res.text)[1]
-      console.log({
-      '_token' : csrf,
-      'login' : '',
-      'username' : username,
-      'password' : password
-      })
-      this.agent
-      .post(`${this.host}/login`)
-      .send({
-        '_token' : csrf,
-        'login' : '',
-        'username' : username,
-        'password' : password
-      })
+      .get(`${this.host}/login`)
       .then(res => {
-        console.log(res.header)
+        let csrf_re = /_token : "([\S]+)",/g
+        let csrf = csrf_re.exec(res.text)[1]
+        let salt_re = /password : md5\(\$\('#input-password'\).val\(\), "([\S]+)"\)/g
+        let salt = salt_re.exec(res.text)[1]
+        console.log({
+          '_token' : csrf,
+          'login' : '',
+          'username' : username,
+          'password' : password
+        })
+        this.agent
+        .post(`${this.host}/login`)
+        .send({
+          '_token' : csrf,
+          'login' : '',
+          'username' : username,
+          'password' : password
+        })
+        .then(res => {
+          console.log(res.header)
+        })
       })
-    })
     }
+  /**
+   * 上传题目
+   * @param {int} problem 题目编号
+   * @param {path} destination 目标目录
+   */
   async upload(problem, destination) {
     let info = JSON.parse(fs.readFileSync(path.join(destination, 'main.json')))
     let token = null
+    // 获取操作的 token
     await this.agent
-    .get(`${this.host}/problem/${problem}/manage/statement`)
-    .then(res => {
-      let $ = JQ(res.text)
-      token = $('[name=_token]').attr('value')
-    })
-    await this.agent
-    .post(`${this.host}/problem/${problem}/manage/statement`)
-    .type('form')
-    .send({
-      _token: token,
-      problem_title: info.title,
-      problem_tags: '',
-      problem_content_md: info.markdown,
-      problem_is_hidden: 'on',
-      'save-problem': '',
+      .get(`${this.host}/problem/${problem}/manage/statement`)
+      .then(res => {
+        let $ = JQ(res.text)
+        token = $('[name=_token]').attr('value')
       })
-    .then(res => {
-      if (res.header['content-encoding'] == 'gzip') {
-      this.throw_error('题面上传失败')
-      }
-      this.console_log('题面上传完毕')
-    })
+    // 上传题面
     await this.agent
-    .post(`${this.host}/problem/${problem}/manage/data`)
-    .attach('problem_data_file', path.join(destination, 'data.zip'))
-    .field('problem_data_file_submit', 'submit')
-    .then(res => {
-      this.console_log('数据上传完毕')
-    })
-    await this.agent
-    .post(`${this.host}/problem/${problem}/manage/data`)
-    .type('form')
-    .send({
-      _token: token,
-      'submit-data': 'data',
+      .post(`${this.host}/problem/${problem}/manage/statement`)
+      .type('form')
+      .send({
+        _token: token,
+        problem_title: info.title,
+        problem_tags: '',
+        problem_content_md: info.markdown,
+        problem_is_hidden: 'on',
+        'save-problem': '', // 注意当且仅当添加此元素才会触发后端的 save 方法
       })
-    .then(res => {
-      this.console_log('数据更新完毕')
-    })
+      .then(res => {
+        if (res.header['content-encoding'] == 'gzip') {
+          this.throw_error('题面上传失败')
+        }
+        this.console_log('题面上传完毕')
+      })
+    // 上传数据
+    await this.agent
+      .post(`${this.host}/problem/${problem}/manage/data`)
+      .attach('problem_data_file', path.join(destination, 'data.zip'))
+      .field('problem_data_file_submit', 'submit') // 注意当且仅当添加此元素才会触发后端的 upload 方法
+      .then(res => {
+        this.console_log('数据上传完毕')
+      })
+    // 刷新后台数据状态
+    await this.agent
+      .post(`${this.host}/problem/${problem}/manage/data`)
+      .type('form')
+      .send({
+        _token: token,
+        'submit-data': 'data',
+      })
+      .then(res => {
+        this.console_log('数据更新完毕')
+      })
   }
+  /**
+   * constructor
+   * @param {string} name OJ 名称
+   * @param {url} host OJ 地址
+   * @param {string} username 用户名，如果不登录则无需配置
+   * @param {string} password 密码，如果不登录则无需配置
+   * @param {string} sessid 如果不使用 cookies 登录则无需配置
+   * @param {string} token 如果不使用 cookies 登录则无需配置
+   * @param {string} token_checksum 如果不使用 cookies 登录则无需配置
+   */
   constructor(name, host, username, password, sessid, token, token_checksum) {
     this.name = name;
     this.host = host;
     this.agent = new superagent.agent();
     if (token && token_checksum) {
-    this.agent
-      .set('Cookie', `uoj_remember_token=${token};uoj_remember_token_checksum=${token_checksum};UOJSESSID=${sessid}`);
+      this.agent
+        .set('Cookie', `uoj_remember_token=${token};uoj_remember_token_checksum=${token_checksum};UOJSESSID=${sessid}`);
     } else if (username && password) {
-    this.login(username, password);
+      this.login(username, password);
     }
   }
   }
